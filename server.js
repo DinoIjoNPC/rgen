b15ff6e79bb32f05c35711616410a4820b3a0f5e
DinoIjoNPC <bielnpcbaik@gmail.com>
2026-07-12 15:13:13 +0800

    Implement CORS middleware in server.js
    Added CORS middleware to handle preflight requests and set headers.

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const PORT = process.env.PORT || 3000;
 const GROK_API_KEY = process.env.GROK_API_KEY;
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/api/generate-image', async (req, res) => {
